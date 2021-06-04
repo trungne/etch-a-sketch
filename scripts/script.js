8acd3b2ad3e4e032f1e2d4fc2f657467e900f91b
trungne <nguyenquochoangtrung@gmail.com>
2021-06-04 16:29:15 +0700

    big bang

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+let GRID_WIDTH = 500; // 300px
+
+function displayGrid(){
+    const container = document.getElementById("container");
+
+    // remove all previous displayed mini grids
+    while(container.firstChild){
+        container.removeChild(container.lastChild);
+    }
+    
+    // set new dimension for grid container
+    const columnNum = document.getElementById("dimension").value;
+    let columnSize = String(`${GRID_WIDTH/columnNum}px `);
+    let gridTemplateColumns = columnSize.repeat(columnNum);
+    container.style.gridTemplateColumns = gridTemplateColumns;
+
+    // create a square the size of columnNum x columnNum
+    for (let i = 0; i < columnNum * columnNum; i++){
+        let grid = document.createElement("div");
+        grid.classList.add("grid-item");
+
+        // minigrid should be a square!
+        grid.style.width = columnSize;
+        grid.style.height = columnSize;
+
+        container.appendChild(grid);
+    }
+}
